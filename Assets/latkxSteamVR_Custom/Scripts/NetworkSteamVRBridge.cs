c8e0ee8bf03a2630a904568b8062854e36cc865a
n1ckfg <nick@fox-gieg.com>
2019-02-21 13:38:31 -0500

    refactor

---
+++ Assets/latkxSteamVR_Custom/Scripts/NetworkSteamVRBridge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkSteamVRBridge : MonoBehaviour {
+
+    public SteamVR_NewController mainCtl;
+    public SteamVR_NewController altCtl;
+    public ContourOscToLatk osc;
+
+    private void Start() {
+		
+	}
+	
+	private void Update() {
+        if (mainCtl.menuPressed && altCtl.padDown || mainCtl.menuDown && altCtl.padPressed) {
+            osc.armReceiver = true;
+        } else if (mainCtl.menuPressed && altCtl.padUp || mainCtl.menuUp && altCtl.padPressed) {
+            osc.armReceiver = false;
+        }
+
+        if (altCtl.triggerDown || altCtl.padDown) osc.clearList();
+    }
+
+}
---
+++ Assets/latkxSteamVR_Custom/Scripts/WacomSteamVRBridge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WacomSteamVRBridge : MonoBehaviour {
+
+    public WacomOffset wacom;
+    public SteamVR_NewController mainCtl;
+
+	private void Update() {
+        if (wacom.isActive) {
+            mainCtl.triggerDown = wacom.triggerDown;
+            mainCtl.triggerPressed = wacom.triggerPressed;
+            mainCtl.triggerUp = wacom.triggerUp;
+
+            mainCtl.menuDown = wacom.menuDown;
+            mainCtl.menuPressed = wacom.menuPressed;
+            mainCtl.menuUp = wacom.menuUp;
+        }
+    }
+
+}
