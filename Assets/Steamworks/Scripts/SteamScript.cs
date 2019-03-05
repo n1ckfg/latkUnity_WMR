54088e64596ce9b2f50ec58345122b3fce002b8c
n1ckfg <nick@fox-gieg.com>
2019-03-04 23:08:21 -0500

    sample steamworks script

---
+++ Assets/Steamworks/Scripts/SteamScript.cs
@@ -0,0 +1,35 @@
+﻿// Import all the necessary namespaces
+using System;
+using System.Text;
+using System.Collections;
+using Steamworks;
+using UnityEngine;
+
+public class SteamScript : MonoBehaviour {
+
+    private IEnumerator Start() {
+        Debug.Log("Steam init: " + GetSteamAuthTicket());
+        yield return null;
+    }
+
+    //This method returns
+    public string GetSteamAuthTicket() {
+        byte[] ticketBlob = new byte[1024];
+        uint ticketSize;
+
+        // Retrieve ticket; hTicket should be a field in the class so you can use it to cancel the ticket later
+        // When you pass an object, the object can be modified by the callee. This function modifies the byte array you've passed to it.
+        HAuthTicket hTicket = SteamUser.GetAuthSessionTicket(ticketBlob, ticketBlob.Length, out ticketSize);
+
+        // Resize the buffer to actual length
+        Array.Resize(ref ticketBlob, (int)ticketSize);
+
+        // Convert bytes to string
+        StringBuilder sb = new StringBuilder();
+        foreach (byte b in ticketBlob) {
+            sb.AppendFormat("{0:x2}", b);
+        }
+        return sb.ToString();
+    }
+
+}
